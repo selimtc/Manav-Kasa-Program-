6b9ed54ad450d1c42f850def090673059f0de257
selimtc <112793794+selimtc@users.noreply.github.com>
2022-12-06 22:22:23 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,39 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    
+		double Armut,Elma,Domates,Muz,Patlıcan, toplam;
+		double ArmutkgFiyat=2.14,ElmakgFiyat= 3.67,DomateskgFiyat =1.11,MuzkgFiyat= 0.95, PatlıcankgFiyat= 5.00;
+		
+		Scanner inp = new Scanner(System.in);
+		
+		System.out.print("Armut Kaç Kilo  : ");
+		Armut = inp.nextInt();
+		
+		System.out.print("Elma Kaç Kilo : ");
+		Elma= inp.nextInt();
+		
+		System.out.print("Domates Kaç Kilo : ");
+		Domates= inp.nextInt();
+		
+		System.out.print("Muz Kaç Kilo : ");
+		Muz= inp.nextInt();
+		
+		System.out.print("Patlıcan Kaç Kilo : ");
+		Patlıcan= inp.nextInt();
+	
+		toplam =Armut*ArmutkgFiyat+Elma*ElmakgFiyat+Domates*DomateskgFiyat+Muz*MuzkgFiyat+Patlıcan*PatlıcankgFiyat;
+		System.out.println("toplam tutar: " + toplam);
+	
+	}
+}
+
